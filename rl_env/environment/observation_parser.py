36b40f4c86932626e710bc60842f5e0f03405532
Mikhail Eysvald <e_misha_vl@mail.ru>
2024-04-24 05:29:33 +0000

    Implemented sample observation parser
    And a small number of unit tests that can be run with Bazel

--- rl_env/environment/observation_parser.py
+++ rl_env/environment/observation_parser.py
@@ -1,4 +1,4 @@
-from gymnasium import spaces
+import math
 
 class ObservationParser:
     task_metrics = (
@@ -12,7 +12,12 @@ class ObservationParser:
     )
     def parse(self, metrics):
         raise NotImplementedError(f"Don't use base {self.__class__.__name__} to parse metrics")
-    
+
+def ln_capped(num, cap):
+        if num == 0:
+            return 0.0
+        ln_value = math.log(num)
+        return ln_value if ln_value <= cap else cap    
 
 class LnCapObservationParser(ObservationParser):
     """
@@ -28,11 +33,18 @@ class LnCapObservationParser(ObservationParser):
         * `vsize_cap` — a cap for vsize. If logarithm of vsize is larger, cap value will be returned
         instead.
         """
-        self.runqueue_cutoff_length = runqueue_cutoff_length
-        self.time_cap = time_cap
-        self.vsize_cap = vsize_cap
+        self.runqueue_cutoff_length = int(runqueue_cutoff_length)
+        self.time_cap = float(time_cap)
+        self.vsize_cap = float(vsize_cap)
 
-    def parse(self, metrics):
+
+    def reshape(self, metrics):
+        """
+        Reshapes metrics into dictionaries while zero-padding or trimming variable-length data. 
+        Reshaping into dictionaries in not strictly necessary and should be avoided
+        in high-performance environments, but this makes metrics easier to read by adding names
+        to them. However, turning variable-length data into fixed-length data is required anyway.
+        """
         result = {
             "callback_type": metrics[0],
             "task_metrics": {},
@@ -40,7 +52,7 @@ class LnCapObservationParser(ObservationParser):
         }
         for elem in result["runqueue"]:
             for metric_name in self.task_metrics:
-                elem[metric_name] = 0.0 # int vs double ??
+                elem[metric_name] = 0.0
         metric_index = 1
         metrics_per_task = len(self.task_metrics)
         for metric_name in self.task_metrics:
@@ -48,7 +60,33 @@ class LnCapObservationParser(ObservationParser):
             metric_index += 1
         while metric_index < len(metrics):
             runqueue_index = (metric_index - (metrics_per_task + 1)) // metrics_per_task
+            if runqueue_index >= len(result["runqueue"]):
+                break
             for metric_name in self.task_metrics:
                 result["runqueue"][runqueue_index][metric_name] = metrics[metric_index]
                 metric_index += 1
         return result
+    
+
+    def cap(self, task_metrics_dict):
+        task_metrics_dict["utime"] = ln_capped(task_metrics_dict["utime"], self.time_cap)
+        task_metrics_dict["stime"] = ln_capped(task_metrics_dict["stime"], self.time_cap)
+        task_metrics_dict["guest_time"] = ln_capped(task_metrics_dict["guest_time"], self.time_cap)
+        task_metrics_dict["vsize"] = ln_capped(task_metrics_dict["vsize"], self.vsize_cap)
+
+
+    def transform(self, reshaped_metrics):
+        """
+        Transforms individual metrics (in this case — by taking a logarithm and capping the result).
+        """
+        self.cap(reshaped_metrics["task_metrics"])
+        for task_metrics_dict in reshaped_metrics["runqueue"]:
+            self.cap(task_metrics_dict)
+        return reshaped_metrics
+    
+
+    def parse(self, metrics):
+        reshaped_metrics = self.reshape(metrics)
+        transformed_metrics = self.transform(reshaped_metrics)
+        return transformed_metrics
+
---
+++ rl_env/tests/observation_parser_tests.py
@@ -0,0 +1,245 @@
+import unittest
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
+from environment.observation_parser import LnCapObservationParser
+
+class TestUnitTests(unittest.TestCase):
+    def test_reshape_runqueue_length_equal_to_cutoff(self):
+        parser = LnCapObservationParser(3, 1, 1)
+        metrics = [
+            1, 
+            0, 5, 0, 1515215, 0, 0, 31512800,
+            0, 5, 0, 1515215, 0, 0, 31512800,
+            2, 1, 1, 778502144, 226203100, 112112112, 595121352,
+            0, 1, 0, 0, 0, 0, 828858368, 
+        ]
+
+        reshaped_metrics = parser.reshape(metrics)
+        self.assertEqual(
+            reshaped_metrics,
+            {
+                "callback_type": 1,
+                "task_metrics": {
+                    "run_state": 0,
+                    "cpu_num": 5,
+                    "preempted": 0,
+                    "utime": 1515215,
+                    "stime": 0,
+                    "guest_time": 0,
+                    "vsize": 31512800,
+                },
+                "runqueue": [
+                    {
+                        "run_state": 0,
+                        "cpu_num": 5,
+                        "preempted": 0,
+                        "utime": 1515215,
+                        "stime": 0,
+                        "guest_time": 0,
+                        "vsize": 31512800,
+                    },
+                    {
+                        "run_state": 2,
+                        "cpu_num": 1,
+                        "preempted": 1,
+                        "utime": 778502144,
+                        "stime": 226203100,
+                        "guest_time": 112112112,
+                        "vsize": 595121352,
+                    },
+                    {
+                        "run_state": 0,
+                        "cpu_num": 1,
+                        "preempted": 0,
+                        "utime": 0,
+                        "stime": 0,
+                        "guest_time": 0,
+                        "vsize": 828858368,
+                    },
+                ]
+            }
+        )
+
+    def test_reshape_runqueue_length_more_than_cutoff(self):
+        parser = LnCapObservationParser(1, 1, 1)
+        metrics = [
+            1, 
+            0, 1, 2, 3, 4, 5, 6,
+            0, 1, 2, 3, 4, 5, 6,
+            7, 8, 9, 10, 11, 12, 13,
+        ]
+
+        reshaped_metrics = parser.reshape(metrics)
+        self.assertEqual(
+            reshaped_metrics,
+            {
+                "callback_type": 1,
+                "task_metrics": {
+                    "run_state": 0,
+                    "cpu_num": 1,
+                    "preempted": 2,
+                    "utime": 3,
+                    "stime": 4,
+                    "guest_time": 5,
+                    "vsize": 6,
+                },
+                "runqueue": [
+                    {
+                        "run_state": 0,
+                        "cpu_num": 1,
+                        "preempted": 2,
+                        "utime": 3,
+                        "stime": 4,
+                        "guest_time": 5,
+                        "vsize": 6,
+                    },
+                ]
+            }
+        )
+
+    def test_reshape_runqueue_length_less_than_cutoff(self):
+        parser = LnCapObservationParser(2, 1, 1)
+        metrics = [
+            1, 
+            0, 1, 2, 3, 4, 5, 6,
+            7, 8, 9, 10, 11, 12, 13,
+        ]
+
+        reshaped_metrics = parser.reshape(metrics)
+        self.assertEqual(
+            reshaped_metrics,
+            {
+                "callback_type": 1,
+                "task_metrics": {
+                    "run_state": 0,
+                    "cpu_num": 1,
+                    "preempted": 2,
+                    "utime": 3,
+                    "stime": 4,
+                    "guest_time": 5,
+                    "vsize": 6,
+                },
+                "runqueue": [
+                    {
+                        "run_state": 7,
+                        "cpu_num": 8,
+                        "preempted": 9,
+                        "utime": 10,
+                        "stime": 11,
+                        "guest_time": 12,
+                        "vsize": 13,
+                    },
+                    {
+                        "run_state": 0,
+                        "cpu_num": 0,
+                        "preempted": 0,
+                        "utime": 0,
+                        "stime": 0,
+                        "guest_time": 0,
+                        "vsize": 0,
+                    },
+                ]
+            }
+        )
+
+    def test_transform(self):
+        parser = LnCapObservationParser(1, 4, 3)
+        reshaped_metrics = {
+            "callback_type": 2,
+            "task_metrics": {
+                "run_state": 11,
+                "cpu_num": 22,
+                "preempted": 33,
+                "utime": 44,
+                "stime": 55,
+                "guest_time": 66,
+                "vsize": 77,
+            },
+            "runqueue": [
+                {
+                    "run_state": 77,
+                    "cpu_num": 66,
+                    "preempted": 55,
+                    "utime": 55,
+                    "stime": 0,
+                    "guest_time": 33,
+                    "vsize": 11,
+                },
+                {
+                    "run_state": 0,
+                    "cpu_num": 5,
+                    "preempted": 1,
+                    "utime": 1,
+                    "stime": 100,
+                    "guest_time": 3,
+                    "vsize": 0,
+                }
+            ]
+        }
+
+        transformed_metrics = parser.transform(reshaped_metrics)
+        expected_result = {
+            "callback_type": 2,
+            "task_metrics": {
+                "run_state": 11,
+                "cpu_num": 22,
+                "preempted": 33,
+                "utime": 3.7841896,
+                "stime": 4.0,
+                "guest_time": 4.0,
+                "vsize": 3.0,
+            },
+            "runqueue": [
+                {
+                    "run_state": 77,
+                    "cpu_num": 66,
+                    "preempted": 55,
+                    "utime": 4.0,
+                    "stime": 0.0,
+                    "guest_time": 3.4965076,
+                    "vsize": 2.3978953,
+                },
+                {
+                    "run_state": 0,
+                    "cpu_num": 5,
+                    "preempted": 1,
+                    "utime": 0.0,
+                    "stime": 4.0,
+                    "guest_time": 1.0986123,
+                    "vsize": 0.0,
+                }
+            ]
+        }
+        self.assertEqual(transformed_metrics["callback_type"], expected_result["callback_type"])
+        self.assertEqual(transformed_metrics["callback_type"], reshaped_metrics["callback_type"])
+        for key in expected_result["task_metrics"].keys():
+            self.assertAlmostEqual(
+                transformed_metrics["task_metrics"][key], 
+                expected_result["task_metrics"][key],
+                delta=1e-6,
+            )
+            self.assertAlmostEqual(
+                transformed_metrics["task_metrics"][key], 
+                reshaped_metrics["task_metrics"][key],
+                delta=1e-6,
+            )
+        for i in range(len(expected_result["runqueue"])):
+            for key in expected_result["runqueue"][i].keys():
+                self.assertAlmostEqual(
+                transformed_metrics["runqueue"][i][key], 
+                expected_result["runqueue"][i][key],
+                delta=1e-6,
+            )
+            self.assertAlmostEqual(
+                transformed_metrics["runqueue"][i][key], 
+                reshaped_metrics["runqueue"][i][key],
+                delta=1e-6,
+            )
+        
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
